edbaf08e3f204c4129221dfb89f7309b2c0e0ae7
Jparlim <Gusjao0500@gmail.com>
2025-06-14 13:54:55 -0300

    teste de commit

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Home } from "../pages/home/main"
+import { Description } from "../pages/descriptions/index"
+import { Pagamento } from "../pages/pagepagamento"
+
 
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/description" element={<Description />} />
+            <Route path="/pagamento" element={<Pagamento />} />
         </Routes>
     )
 }
